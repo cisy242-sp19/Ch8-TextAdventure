8fe722cdff34cd2ea7d7c9c8f6b31e99b9391fbb
Amriganati <30274109+Amriganati@users.noreply.github.com>
2019-04-15 10:06:17 -0400

    build 5.3
    made sure the unimplemented classes as least had a meaningful description. despite never being called.

--- Items.java
+++ Items.java
@@ -1,6 +1,7 @@
 
 /**
- * Enumeration class Items - write a description of the enum class here
+ * Enumeration class Items - would/will be where the enumerations values for items
+ * is stored. In the event that items are handled by a case switch.
  *
  * Andrew Riganati
  * 4.13.2019
